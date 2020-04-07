313e8b02cc4788b263d9347cb2fa4838c599ded7
macbook <macbook@MacBook-Pro-macbook.local>
2020-04-08 01:20:46 +0300

    added awk

---
+++ awk.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+echo "filename to process"
+read filename
+
+if [[ -f $filename ]] 
+then
+   #awk '{print}' $filename
+    awk '/mac/ {print $1,$3}' $filename
+else
+    echo "No file found"
+fi
